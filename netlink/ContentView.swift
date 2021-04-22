4a4807c2af54e8968be471476959cd7c659ad897
sgulls <abaoagjoojin@outlook.com>
2021-04-22 17:35:59 -0400

    Add app project

---
+++ netlink/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  netlink
+//
+//  Created by Joojin Abaoag on 4/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
